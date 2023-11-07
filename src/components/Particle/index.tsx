3f115472f5d0993359d25e1b4a41daca34cbc4a8
Syncxv <aruldevarackal1@gmail.com>
2023-11-07 12:12:00 +1100

    hm

--- src/components/Particle/index.tsx
+++ src/components/Particle/index.tsx
@@ -2,6 +2,24 @@ import { Component, onCleanup, onMount } from 'solid-js';
 import * as THREE from 'three';
 import vertexShader from './shaders/vertex.vert';
 import fragmentShader from './shaders/fragment.frag';
+import System, {
+    SpriteRenderer,
+    Emitter,
+    Rate,
+    Span,
+    Position,
+    Mass,
+    Radius,
+    Life,
+    Velocity,
+    PointZone,
+    Vector3D,
+    Alpha,
+    Scale,
+    Color,
+    RadialVelocity,
+    Body,
+} from 'three-nebula';
 
 let container: HTMLDivElement;
 
@@ -52,7 +70,7 @@ function destroy() {
 }
 
 interface Props {}
-
+let tha = 0;
 export const ParticleThing: Component<Props> = (props) => {
     onMount(() => {
         init();
@@ -77,21 +95,59 @@ export const ParticleThing: Component<Props> = (props) => {
         const curveObject = new THREE.Line(geometry, lineMaterial);
         scene.add(curveObject);
 
+        // thank you for your service circle :salute:
         const circleGeometry = new THREE.CircleGeometry(0.05, 32);
         const circleMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
         const circle = new THREE.Mesh(circleGeometry, circleMaterial);
 
-        circle.position.copy(points[0]);
+        // circle.position.copy(points[0]);
 
-        scene.add(circle);
+        // scene.add(circle);
+
+        const system = new System();
+        const emitter = new Emitter();
+        const spriteRenderer = new SpriteRenderer(scene, THREE);
+
+        // Set emitter rate (particles per second) as well as the particle initializers and behaviours
+        emitter
+            // .setRate(new Rate(new Span(4, 16), new Span(0.01)))
+            .setRate(new Rate(20, 0.01))
+            .setInitializers([
+                new Position(new PointZone(0, 0)),
+                new Mass(1),
+                new Radius(12, 24),
+                new RadialVelocity(1, new Vector3D(0, 0.1, 0), 180),
+                new Life(3), // hi
+                new Body(circle),
+            ])
+            .setBehaviours([
+                new Alpha(1, 0), // bruuuh
+                new Scale(0.1, 0.1),
+                new Color(new THREE.Color(0xf00fff), new THREE.Color(0xffffff)),
+            ])
+            .setPosition({ x: 0, y: 0 })
+
+            .emit();
+
+        // add the emitter and a renderer to your particle system
+        system
+            .addEmitter(emitter)
+            .addRenderer(spriteRenderer)
+            .emit({
+                onStart: () => {},
+                onUpdate: () => {},
+                onEnd: () => {},
+            });
 
         function animate(idk?: number) {
             const elapsedTime = clock.getElapsedTime();
             const t = ((elapsedTime * 0.3) % 2) / 2;
 
             const positionOnCurve = curve.getPointAt(t);
-            circle.position.copy(positionOnCurve);
+            // circle.position.copy(positionOnCurve);
+            emitter.position.copy(positionOnCurve as any);
 
+            system.update();
             renderer.render(scene, camera);
             animateId = requestAnimationFrame(animate);
         }
